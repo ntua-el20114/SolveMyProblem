202ddf1ade69e2ec6dfc3aa67132dd7624682ec3
ntua-el20114 <el20114@mail.ntua.gr>
2024-07-01 16:23:59 +0300

    Implemented problem results page

--- ms_results/routes/index.js
+++ ms_results/routes/index.js
@@ -1,4 +1,7 @@
-var express = require('express');
+const express = require('express');
+const db = require('../services/database');
+var initModels = require("../models/init-models");
+
 var router = express.Router();
 const db = require('../services/database');
 var initModels = require("../models/init-models");
@@ -8,6 +11,28 @@ let models;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/results', async (req, res) => {
+  try {
+    sequelize = await db.getSequelizeInstance();
+    models = initModels(sequelize);
+    const result = await models.Results.create(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get('/results', async (req, res) => {
+  try {
+    sequelize = await db.getSequelizeInstance();
+    models = initModels(sequelize);
+    const results = await models.Results.findAll();
+    res.status(200).json(results);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.post('/delete-problem', async (req, res) => {
